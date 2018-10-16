74fecdc9b0e69762eb34796aa10d44d84d0a4916
Robert Vrabel <rvrabel@gmail.com>
2018-10-16 14:29:14 -0400

    Initial import

---
+++ src/ThemeServiceProvider.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Waynestate\NovaTheme;
+
+use Laravel\Nova\Nova;
+use Laravel\Nova\Events\ServingNova;
+use Illuminate\Support\ServiceProvider;
+
+class ThemeServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap any application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        Nova::serving(function (ServingNova $event) {
+            Nova::style('nova-theme', __DIR__.'/../resources/css/theme.css');
+        });
+    }
+}
